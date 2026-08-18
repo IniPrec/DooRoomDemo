498e2c541d613fe778485248ddc12a7cd0222a1a
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-18 13:33:02 +0100

    Hardcoded a list in the room controller

---
+++ DooRoomDemo/Controllers/RoomController.cs
@@ -0,0 +1,34 @@
+﻿using DooRoomDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DooRoomDemo.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Room> room = new List<Room>
+            {
+                new Room
+                {
+                    Name = "Study room",
+                    Type = "Study",
+                    IsUnlocked = false,
+                },
+                new Room
+                {
+                    Name = "Kitchen",
+                    Type = "wash, dishes",
+                    IsUnlocked = false
+                },
+                new Room
+                {
+                    Name = "Gym",
+                    Type = "exercise",
+                    IsUnlocked = false
+                }
+            };
+            return View();
+        }
+    }
+}
