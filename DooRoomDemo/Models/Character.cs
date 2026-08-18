5d734c8887f61419927a44d78c85753524726eb8
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-18 13:24:18 +0100

    Added a character model

---
+++ DooRoomDemo/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace DooRoomDemo.Models
+{
+    public class Character
+    {
+        public string? Name { get; set; }
+
+        public int Level { get; set; }
+
+        public int StreakCount { get; set; }
+    }
+}
