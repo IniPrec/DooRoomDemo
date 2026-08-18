5f331e484d1a651616127a5c40fd57f04e6cce3a
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-18 12:32:14 +0100

    Built the controller for the application

---
+++ DooRoomDemo/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using DooRoomDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DooRoomDemo.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TaskItem> taskItems = new List<TaskItem>
+            {
+                new TaskItem {
+                TaskName = "Study",
+                TaskDescription = "Study for the exam",
+                Keywords = new List<string> { "study", "exam" },
+                TaskStatus = false
+            },
+                new TaskItem {
+                TaskName = "Exercise",
+                TaskDescription = "Go to the gym",
+                Keywords = new List<string> { "exercise", "gym" },
+                TaskStatus = false
+            },
+                new TaskItem {
+                TaskName = "Grocery Shopping",
+                TaskDescription = "Buy groceries for the week",
+                Keywords = new List<string> { "grocery", "shopping" },
+                TaskStatus = false
+                }
+            };
+            return View(taskItems);
+        }
+    }
+}
