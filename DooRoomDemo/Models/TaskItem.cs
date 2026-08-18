48e49cc1aef89471d61036c7657feb940b948df7
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-18 12:31:21 +0100

    Created properties in the Task Item class

---
+++ DooRoomDemo/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DooRoomDemo.Models
+{
+    public class TaskItem
+    {
+        public Guid TaskId { get; set; }
+
+        public string? TaskName { get; set; }
+
+        public string? TaskDescription { get; set; }
+
+        public List<string>? Keywords { get; set; }
+
+        public bool TaskStatus { get; set; }
+    }
+}
