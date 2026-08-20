9b62cccdd50ad98eaa0ca8f567ef61e964f87166
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-20 08:33:08 +0100

    Filing in the house controller

---
+++ DooRoomDemo/Controllers/HouseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DooRoomDemo.Models;
+
+namespace DooRoomDemo.Controllers
+{
+    public class HouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            House house = new House()
+            {
+                Id = Guid.NewGuid(),
+                Rooms = new List<Room>
+                {
+                    new Room
+                    {
+                        Name = "Study room",
+                        Type = new List<string> { "Study" },
+                        IsUnlocked = false,
+                    },
+                    new Room
+                    {
+                        Name = "Kitchen",
+                        Type = new List<string> { "wash, dishes" },
+                        IsUnlocked = false
+                    },
+                    new Room
+                    {
+                        Name = "Gym",
+                        Type = new List < string > { "exercise" },
+                        IsUnlocked = false
+                    }
+                }
+            };
+            return View(house);
+        }
+    }
+}
